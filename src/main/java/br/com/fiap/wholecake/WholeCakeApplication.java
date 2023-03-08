7f02b7e0574719bcf5355f2118b70a39ca90b90d
mariananieton <rm94141@fiap.com.br>
2023-03-08 12:17:07 -0300

    Aplicação criada no Spring Initialzr

---
+++ src/main/java/br/com/fiap/wholecake/WholeCakeApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.wholecake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WholeCakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WholeCakeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/wholecake/WholeCakeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.wholecake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WholeCakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
